e42b9242163daae2da418d46dfba488c78c15653
bbkelleroglu <burakkelleroglu@gmail.com>
2020-03-17 15:43:41 +0300

    implement BindableType protocol to track mvvm structure

---
+++ VNGRS-MovieAPP/BindableType.swift
@@ -0,0 +1,17 @@
+//
+//  BindableType.swift
+//  VNGRS-MovieAPP
+//
+//  Created by Burak Kelleroğlu on 17.03.2020.
+//  Copyright © 2020 Burak Kelleroğlu. All rights reserved.
+//
+
+import Foundation
+
+protocol BindableType {
+    associatedtype ViewModelType
+
+    var viewModel: ViewModelType! { get set }
+
+    func bindViewModel()
+}
