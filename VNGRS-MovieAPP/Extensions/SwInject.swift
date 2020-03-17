2d3cc442331ce14fc84519008d4a9ddc33acd655
bbkelleroglu <burakkelleroglu@gmail.com>
2020-03-17 11:16:29 +0300

    implement Mock scheme to work mock services.

---
+++ VNGRS-MovieAPP/Extensions/SwInject.swift
@@ -0,0 +1,24 @@
+//
+//  SwInject.swift
+//  VNGRS-MovieAPP
+//
+//  Created by Burak Kelleroğlu on 17.03.2020.
+//  Copyright © 2020 Burak Kelleroğlu. All rights reserved.
+//
+
+import Foundation
+import Swinject
+import SwinjectAutoregistration
+
+extension Container {
+    static let shared: Container = {
+        let container = Container()
+        registerMockServices(into: container)
+        return container
+
+    }()
+    //TODO: register real services.
+    private static func registerMockServices(into container: Container) {
+        container.autoregister(MovieMock.self, initializer: MovieMock.init)
+    }
+}
--- VNGRS-MovieAPP/Services/MockService/MovieMock.swift
+++ VNGRS-MovieAPP/Services/MockService/MovieMock.swift
@@ -7,10 +7,9 @@
 //
 
 import Foundation
-#if MOCK
+//TODO: #if MOCK
 class MovieMock {
-    let movie = Movie(popularity: 52.277, voteCount: 2705, posterPath: "https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UY268_CR1,0,182,268_AL_.jpg", title: "Fast & Furious Presents: Hobbs & Shaw", voteAverage: "6.6", releaseDate: "2019-08-01")
+    let movie = Movie(popularity: 52.277, voteCount: 2705, posterPath: "https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UY268_CR1,0,182,268_AL_.jpg", title: "Fast & Furious Presents: Hobbs & Shaw", voteAverage: 6.6, releaseDate: "2019-08-01")
     lazy private(set) var movies = [movie, movie, movie]
 }
 
-#endif
